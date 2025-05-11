d3ee21001efce71ab7cc9598a0e9033c191dd786
zcat07 <143707195+ganesh9714@users.noreply.github.com>
2025-05-11 20:13:13 +0530

    local-host-fixes

--- Hope_Hospitals/settings.py
+++ Hope_Hospitals/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# CSRF Settings
+# CSRF_TRUSTED_ORIGINS = ['https://hospital1975.pythonanywhere.com', '*']
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
 
 # Application definition
 
@@ -117,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles/novena"),
